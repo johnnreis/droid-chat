3e017e3a0db888fa43cb076583e8dab6e2546bf6
John Reis <jhonatareisy7@gmail.com>
2025-02-05 23:14:30 -0300

    feat: create splash screen and configure flow of router

--- app/src/main/java/br/com/droidchat/MainActivity.kt
+++ app/src/main/java/br/com/droidchat/MainActivity.kt
@@ -4,13 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import br.com.droidchat.navigation.ChatNavHost
+import br.com.droidchat.ui.ChatApp
 import br.com.droidchat.ui.theme.DroidChatTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +19,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DroidChatTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                 DroidChatTheme {
+                     ChatApp()
+                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DroidChatTheme {
-        Greeting("Android")
-    }
-}
---
+++ app/src/main/java/br/com/droidchat/ui/feature/splash/SplashScreen.kt
@@ -0,0 +1,79 @@
+package br.com.droidchat.ui.feature.splash
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import br.com.droidchat.R
+import br.com.droidchat.ui.theme.BackgroundGradient
+import br.com.droidchat.ui.theme.DroidChatTheme
+
+
+@Composable
+fun SplashRoute() {
+    SplashScreen()
+}
+
+@Composable
+fun SplashScreen() {
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(brush = BackgroundGradient)
+            .padding(16.dp),
+
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = null
+        )
+
+        Spacer(modifier = Modifier.height(77.dp))
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_safety),
+                contentDescription = null,
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+
+            val context = LocalContext.current
+            Text(
+                text = context.getString(R.string.splash_safety_info),
+                textAlign = TextAlign.Center,
+                color = Color.White,
+                style = MaterialTheme.typography.bodyMedium,
+            )
+
+        }
+    }
+}
+
+@Composable
+@Preview
+fun SplashScreenPreview() {
+    DroidChatTheme {
+        SplashScreen()
+    }
+}
