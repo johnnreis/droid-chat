d357d7f4a24a86ece171589c846774e9589cf20f
John Reis <jhonatareisy7@gmail.com>
2025-02-14 10:01:43 -0300

    feat: att dependecies

---
+++ app/src/main/java/br/com/droidchat/ui/feature/signup/SignUpScreen.kt
@@ -0,0 +1,23 @@
+package br.com.droidchat.ui.feature.signup
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.droidchat.ui.theme.DroidChatTheme
+
+@Composable
+fun SignUpRoute() {
+    SignUpScreen()
+}
+
+@Composable
+fun SignUpScreen() {
+
+}
+
+@Preview
+@Composable
+private fun SignUpScreenPreview() {
+    DroidChatTheme {
+        SignUpScreen()
+    }
+}
