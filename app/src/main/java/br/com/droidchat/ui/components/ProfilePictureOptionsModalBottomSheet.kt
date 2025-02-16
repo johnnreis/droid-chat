da7cc82fccb35937a95eef8f9a14ce19bd22bbe1
John Reis <jhonatareisy7@gmail.com>
2025-02-16 19:10:37 -0300

    style: created style of bottomSheet

---
+++ app/src/main/java/br/com/droidchat/ui/components/ProfilePictureOptionsModalBottomSheet.kt
@@ -0,0 +1,109 @@
+package br.com.droidchat.ui.components
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.SheetState
+import androidx.compose.material3.SheetValue
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.dp
+import br.com.droidchat.R
+import br.com.droidchat.ui.theme.DroidChatTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ProfilePictureOptionsModalBottomSheet(
+    onDismissRequest: () -> Unit,
+    modifier: Modifier = Modifier,
+    sheetState: SheetState = rememberModalBottomSheetState(),
+) {
+    ModalBottomSheet(
+        onDismissRequest = onDismissRequest,
+        modifier = modifier,
+        sheetState = sheetState,
+        containerColor = MaterialTheme.colorScheme.surface,
+        contentColor = MaterialTheme.colorScheme.onSurface
+    ) {
+
+        ProfilePictureOptionRow(
+            iconResId = R.drawable.ic_photo_camera,
+            textStringId = R.string.common_take_photo,
+            onCLick = {}
+        )
+
+        ProfilePictureOptionRow(
+            iconResId = R.drawable.ic_photo_library,
+            textStringId = R.string.common_upload_photo,
+            onCLick = {}
+        )
+
+    }
+}
+
+@Composable
+private fun ProfilePictureOptionRow(
+    @DrawableRes
+    iconResId: Int,
+    textStringId: Int,
+    onCLick: () -> Unit,
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .defaultMinSize(minWidth = 48.dp)
+            .clickable {
+                onCLick()
+            }
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            painter = painterResource(id = iconResId),
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.onSurface
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Text(
+            text = stringResource(id = textStringId),
+            color = MaterialTheme.colorScheme.onSurface,
+            style = MaterialTheme.typography.bodyLarge
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+private fun ProfilePictureOptionsModalBottomSheetPreview() {
+    val sheetState = SheetState(
+        skipPartiallyExpanded = true,
+        density = Density(LocalContext.current),
+        initialValue = SheetValue.Expanded
+    )
+    DroidChatTheme {
+        ProfilePictureOptionsModalBottomSheet(
+            onDismissRequest = {},
+            sheetState = sheetState
+        )
+    }
+}
