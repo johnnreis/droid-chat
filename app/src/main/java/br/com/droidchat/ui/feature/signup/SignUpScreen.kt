170fa46ccc870275cd0db7f47f3236a807e7918c
John Reis <jhonatareisy7@gmail.com>
2025-02-16 13:38:34 -0300

    fix: fixing scrollvertical

--- app/src/main/java/br/com/droidchat/ui/feature/signup/SignUpScreen.kt
+++ app/src/main/java/br/com/droidchat/ui/feature/signup/SignUpScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
@@ -43,8 +42,7 @@ fun SignUpScreen() {
 
     ) {
         Column(
-            modifier = Modifier
-                .fillMaxSize(),
+            modifier = Modifier.fillMaxSize(),
 
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
